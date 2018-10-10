2c74a43aa223f939ef9f776c83992b5750d5a12b
Roman Torres <roman.torres@globant.com>
2018-10-11 00:29:17 +0100

    Add data provider to exercise 2

--- src/test/java/com/globant/training/automation/tests/StarbucksTests.java
+++ src/test/java/com/globant/training/automation/tests/StarbucksTests.java
@@ -4,7 +4,7 @@ import com.globant.training.automation.pages.CoffeeFinderPage;
 import com.globant.training.automation.pages.CoffeeFinderResultPage;
 import com.globant.training.automation.pages.StarbucksHomePage;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -19,6 +19,15 @@ public class StarbucksTests extends BaseTests {
         homePage = getHomePage();
     }
 
+    @DataProvider(name = "coffeeFinderData")
+    public Object[][] dataForFindYourCoffee() {
+        return new Object[][] {
+                {"Lighthearted and sunny", "A group of friends", "Cocoa", "I like things simple"},
+                {"Balanced and easy-going", "A quiet moment", "Citrus", "I can mix it up sometimes"},
+                {"Bold and complex", "A busy day", "Nuts and spices", "I love to try something new and different"}
+        };
+    }
+
     @Test
     public void menu() {
         String[] expectedElements = {"COFFEE", "TEA", "MENU", "COFFEEHOUSE", "SOCIAL IMPACT", "STARBUCKS REWARDS", "BLOG", "GIFT CARDS"};
@@ -29,13 +38,13 @@ public class StarbucksTests extends BaseTests {
         assertThat(menu).asList().contains(expectedElements);
     }
 
-    @Test
-    public void findYourPerfectCoffee() {
+    @Test(dataProvider = "coffeeFinderData")
+    public void findYourPerfectCoffee(String optOne, String optionTwo, String optionThree, String optionFour) {
         CoffeeFinderPage page = homePage.clickFindYourPerfectCoffee();
-        page.setOptionQuestionOne("Lighthearted and sunny");
-        page.setOptionQuestionTwo("A group of friends");
-        page.setOptionQuestionThree("Cocoa");
-        page.setOptionQuestionFour("I like things simple");
+        page.setOptionQuestionOne(optOne);
+        page.setOptionQuestionTwo(optionTwo);
+        page.setOptionQuestionThree(optionThree);
+        page.setOptionQuestionFour(optionFour);
         CoffeeFinderResultPage resultPage = page.clickFindMyCoffee();
         assertThat(resultPage.getCurrentUrl()).isEqualTo("https://athome.starbucks.com/coffee-finder/");
         assertThat(resultPage.isFeaturedCoffesResulDisplayed()).isTrue();
